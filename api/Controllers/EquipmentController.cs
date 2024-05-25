4b459d298dabf669f216bc6dcc6b024bebaf6820
Lai Kok Wui <kokwei325@gmail.com>
2024-05-25 17:55:01 +0800

    update api

---
+++ api/Controllers/EquipmentController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EquipmentController : ControllerBase
+    {
+        // GET: api/Equipment
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Equipment/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Equipment
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Equipment/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/Equipment/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ api/Equipment.cs
@@ -0,0 +1,12 @@
+﻿namespace api
+{
+    public class Equipment
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Condition { get; set; }
+        public int Quantity { get; set; }
+        public int CategoryId { get; set; }
+
+    }
+}
